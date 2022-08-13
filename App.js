d47195ecd307b79ed045622eae007b3613282381
root <you@example.com>
2022-08-13 01:42:08 +0000

    Criei Calculadora

---
+++ App.js
@@ -0,0 +1,56 @@
+import react from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+export default function app(){
+	return(
+	<View style={styles.container}>
+		<View style={styles.bloco}>
+			<Text style={styles.titulo}>Calculadora Simples</Text>
+		</View>
+		<View style={styles.bloco}>
+			<Text> Valor 1; </Text>
+			<TextInput style={styles.input}/>
+		</View>
+		<View style={styles.bloco}>
+			<Text> Valor 2; </Text>
+			<TextInput style={styles.input}/>
+			</View>
+			<View style={styles.bloco}>
+				<TouchableOpacity style={styles.botao}>
+					<Text style={styles.textoBotao}>SOMAR</Text>
+				</TouchableOpacity>
+			</View>
+	</View>
+	)
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:'#FBFF00'
+  },
+	bloco:{
+		marginTop:20,
+		width:'80%',
+		marginLeft:'10%',
+	},
+	titulo:{
+		fontSize:30,
+		textAlign:'center'
+	},
+	input:{
+		borderWidth:2,
+		fontSize:20,
+		borderRadius:10
+	},
+	botao:{
+		backgroundColor:'#000',
+		borderRadius:10
+	},
+	textoBotao:{
+		color:'#fff',
+		textAlign:'center',
+		fontSize:20
+	},
+	
+});

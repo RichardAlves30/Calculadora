318d4eb2cd592881aedcce716f69f4b647facec6
root <you@example.com>
2022-08-20 01:55:56 +0000

    cal2

--- App.js
+++ App.js
@@ -1,25 +1,47 @@
-import react from 'react';
+import react,{useState} from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function app(){
+	const[valor1, setValor1]=useState("20");
+	const[valor2, setValor2]=useState("3");
+	const[resultado, setResultado]=useState();
+	function somar(){
+		let resultado = parseFloat(valor1) + parseFloat(valor2);
+		setResultado(resultado);
+	}
 	return(
 	<View style={styles.container}>
 		<View style={styles.bloco}>
 			<Text style={styles.titulo}>Calculadora Simples</Text>
 		</View>
 		<View style={styles.bloco}>
-			<Text> Valor 1; </Text>
-			<TextInput style={styles.input}/>
+			<Text> Valor 1: </Text>
+			<TextInput style={styles.input}
+				value={valor1}
+				onChangeText={(valor)=>setValor1(valor)}
+				keyBoardType="numeric"
+				/>
 		</View>
 		<View style={styles.bloco}>
-			<Text> Valor 2; </Text>
-			<TextInput style={styles.input}/>
-			</View>
-			<View style={styles.bloco}>
-				<TouchableOpacity style={styles.botao}>
+			<Text> Valor 2: </Text>
+			<TextInput style={styles.input}
+				value={valor2}
+				onChangeText={(valor)=>setValor2(valor)}
+				keyBoardType="numeric"
+				/>
+		</View>
+		<View style={styles.bloco}>
+				<TouchableOpacity style={styles.botao}
+					onPress={somar}
+					>
 					<Text style={styles.textoBotao}>SOMAR</Text>
 				</TouchableOpacity>
-			</View>
+		</View>
+		<View style={styles.bloco}>
+		<Text style={styles.titulo}>
+				RESULTADO:{resultado}
+			</Text>
+		</View>
 	</View>
 	)
 }
@@ -27,7 +49,7 @@ export default function app(){
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'#FBFF00'
+    backgroundColor:'#2828b0'
   },
 	bloco:{
 		marginTop:20,
